2546bf712fa21090f630182d9f78550ed7a2074d
naroto102hn2 <naroto102hn2@gmail.com>
2023-10-05 09:22:20 +0700

    init project with swiftlint

---
+++ AnimeHub/AnimeHub/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  AnimeHub
+//
+//  Created by Tobi on 05/10/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
